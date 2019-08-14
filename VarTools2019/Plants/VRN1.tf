16e6267a41a8527b55d174bc4934b64b89e7074b
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2019-08-14 11:41:45 +0100

    added script and data files

---
+++ VarTools2019/Plants/VRN1.tf
@@ -0,0 +1,33 @@
+AC  AY750993/VRN1/EEADannot
+XX
+ID  AY750993:VRN1:EEADannot
+XX
+NA  AY750993
+XX
+DE  AY750993
+XX
+OS  Hordeum vulgare cv Strider
+XX
+BF  10127;
+XX
+P0      A      C      G      T
+01     19     72      7      2      C
+02     10     71      7     12      C
+03     57     17     14     12      a
+04     56      6     32      6      r
+05     94      2      4      0      A
+06     84      1      3     12      A
+07     79      7      7      7      A
+08     57      4      9     30      w
+09     24      0     76      0      G
+10     15      0     85      0      G
+XX
+LN  http://floresta.eead.csic.es/footprintdb/index.php?motif=AY750993:VRN1:EEADannot
+XX
+RN  [1];
+RL  Deng W, Casao Mc, Wang P, Sato K, Hayes PM, Jean Finnegan E, Trvaskis B (2015) Direct links between the vernalization response and other key traits of cereal crops. Nat Comm 6:5882
+RN  [2];
+RX  PUBMED: 24234003
+RL  Sebastian, A., Contreras-Moreira, B. footprintDB: a database of transcription factors with annotated cis elements and binding interfaces. Bioinformatics 30, 258-65 (2014).
+XX
+//
